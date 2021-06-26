e6264574e2c077298e0f2f2a3516f34f18c09382
thomasguy <twguy66@gmail.com>
2021-06-26 08:33:14 +0700

    wide2 1.66

--- gatsby-isolationists/src/utils/helpers.js
+++ gatsby-isolationists/src/utils/helpers.js
@@ -2,7 +2,7 @@ function addClass(ratio) {
   switch (true) {
     case ratio < 0.58: // ratio 29/50
       return 'tall2';
-    case ratio > 1.52:
+    case ratio > 1.66:
       return 'wide2';
     // case ratio > 2.5:
     //   return 'wide3';
