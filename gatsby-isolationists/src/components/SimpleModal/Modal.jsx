6d11f1719481acc9e0754648be68874797b50911
thomasguy <twguy66@gmail.com>
2021-11-21 15:13:09 +0700

    modal onClick put back

--- gatsby-isolationists/src/components/SimpleModal/Modal.jsx
+++ gatsby-isolationists/src/components/SimpleModal/Modal.jsx
@@ -11,7 +11,6 @@ import { ModalBox, ModalWrapper, Title } from './modalStyles';
 
 export function Modal({ onCloseRequest, index, imgProps }) {
   const idxRef = useRef(index);
-  const modal = useRef();
   const [_index, _setIndex] = useState(index);
 
   const pictures = imgProps.map(props => {
@@ -38,52 +37,31 @@ export function Modal({ onCloseRequest, index, imgProps }) {
     [imgProps.length],
   );
 
-  const handleKeypress = useCallback(evt => {
-    if (evt.keyCode === 39) setIndex(idxRef.current + 1);
-    if (evt.keyCode === 37) setIndex(idxRef.current - 1);
-  }, []);
-
-  const handleClick = useCallback(evt => {
-    // console.log('target', evt.target.id);
-    if (
-      evt.target.id === 'i-next' ||
-      evt.target.id === 'next' ||
-      evt.target.parentElement.id === 'next'
-    ) {
-      setIndex(idxRef.current + 1);
-    }
-    if (
-      evt.target.id === 'i-previous' ||
-      evt.target.id === 'previous' ||
-      evt.target.parentElement.id === 'previous'
-    ) {
-      setIndex(idxRef.current - 1);
-    }
-  }, []);
+  const handleKeypress = useCallback(
+    evt => {
+      if (evt.keyCode === 39) setIndex(idxRef.current + 1);
+      if (evt.keyCode === 37) setIndex(idxRef.current - 1);
+    },
+    [setIndex],
+  );
 
   useEffect(() => {
-    const div = modal.current;
-    if (div) {
-      div.addEventListener('click', handleClick);
-      document.addEventListener('keydown', handleKeypress);
+    document.addEventListener('keydown', handleKeypress);
 
-      return () => {
-        div.removeEventListener('click', handleClick);
-        document.removeEventListener('keydown', handleKeypress);
-      };
-    }
-    return [modal];
-  }, [handleClick, handleKeypress]);
+    return () => {
+      document.removeEventListener('keydown', handleKeypress);
+    };
+  }, [handleKeypress]);
 
   return (
-    <ModalWrapper ref={modal}>
+    <ModalWrapper>
       <Title>{imgProps[_index].artist}</Title>
       <CloseModal close={() => onCloseRequest()} />
-      <Previous />
+      <Previous slider={() => setIndex(idxRef.current - 1)} />
       <ModalBox>
         <ModalImg imgProp={imgProps[_index]}>{pictures[_index]}</ModalImg>
       </ModalBox>
-      <Next />
+      <Next slider={() => setIndex(idxRef.current + 1)} />
     </ModalWrapper>
   );
 }
