54703338d7a4167d3ead4c6b77ea87a50a454500
Thomas Guy <TWGuy66@gmail.com>
2024-03-08 11:04:49 +0000

    sanity v3.1

--- sanity-isolationists/sanity.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from '@sanity/client'
-
-const client = createClient({
-  projectId: 'ukbaygfd',
-  dataset: 'production',
-  useCdn: true,
-})
-
-export default client;
--- sanity-isolationists/schema/index.js
+++
@@ -1,8 +0,0 @@
-import artist from './artist';
-import picture from './picture';
-import subject from './subject';
-import link from './link';
-import social from './social';
-import dimensions from './dimensions';
-
-export const schemaTypes = [artist, subject, picture, link, social, dimensions]
